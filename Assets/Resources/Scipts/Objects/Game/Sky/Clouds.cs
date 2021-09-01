b471de93be8b1da8235bf06b5ce30526b6e34fd0
norman-andrians <norman25.na@gmail.com>
2021-09-01 20:55:16 +0700

    version: 0.11, mengatur awan fase-1

--- Assets/Resources/Scipts/Objects/Game/Sky/Clouds.cs
+++ Assets/Resources/Scipts/Objects/Game/Sky/Clouds.cs
@@ -1,21 +1,30 @@
-﻿using UnityEngine;
+﻿/* 
+ * ini adalah skrip untuk menggerakkan awan
+ */
+
+using UnityEngine;
 
 public class Clouds : MonoBehaviour
 {
+    // mengatur minimum dan maximum kecepatan random
     public float minSpeed = 1f;
     public float maxSpeed = 3f;
 
+    // kecepatan awan
     private float speed;
 
     void Start()
     {
+        // mengatur kecepatan random
         speed = Random.Range(minSpeed, maxSpeed);
     }
 
     private void FixedUpdate()
     {
+        // vector 2 menjalankan awan
         Vector2 jalan = new Vector2(-speed, 0);
 
+        // menjalankan awan
         transform.Translate(jalan * Time.deltaTime);
     }
 }
--- Assets/Resources/Scipts/Objects/Game/Sky/CloudsSpawner.cs
+++ Assets/Resources/Scipts/Objects/Game/Sky/CloudsSpawner.cs
@@ -1,20 +1,31 @@
-﻿using System.Collections;
+﻿/*
+ * skrip ini berfungsi untuk mengatur munculnya awan
+ */
+
 using UnityEngine;
 
 public class CloudsSpawner : MonoBehaviour
 {
+    // objek awan
     public GameObject[] awan;
-    public float delay = 2f;
+    // delay munculkan awan
+    public float delay = 20f;
 
+    // posisi vector 2
     private float posx;
     private float posy;
+
+    // waktu tiap frame
+    private float waktu;
 
     // Start is called before the first frame update
     void Start()
     {
+        // mengatur posisi vector 2 awan untuk pertama
         Vector2 pos1 = new Vector2(3f, 4f);
         Vector2 pos2 = new Vector2(4f, -3f);
         
+        // memunculkan awan
         var awan1 = Instantiate(awan[0], pos1, Quaternion.identity);
         var awan2 = Instantiate(awan[1], pos2, Quaternion.identity);
 
@@ -23,23 +34,33 @@ public class CloudsSpawner : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // memulai waktu
+        waktu += Time.deltaTime;
+        // memunculkan awan
+        munculkanAwan();
+
+        Debug.Log("waktu: " + waktu.ToString("F2"));
     }
 
-    IEnumerator munculkanAwan()
+    // function untuk memunculkan awan
+    private void munculkanAwan()
     {
+        // memunculkan awan setiap delay
         while (true)
         {
-            int seedIndex = Random.Range(0, awan.Length);
+            if (waktu%delay == 0)
+            {
+                int seedIndex = Random.Range(0, awan.Length);
 
-            posx = Random.Range(transform.position.x - 10f, transform.position.x + 10f);
-            posy = Random.Range(transform.position.y - 10f, transform.position.y + 10f);
+                posx = Random.Range(transform.position.x - 10f, transform.position.x + 10f);
+                posy = Random.Range(transform.position.y - 10f, transform.position.y + 10f);
 
-            Instantiate(awan[seedIndex], new Vector2(posx, posy), Quaternion.identity);
+                Instantiate(awan[seedIndex], new Vector2(posx, posy), Quaternion.identity);
 
-            yield return new WaitForSeconds(delay);
+                Debug.Log("munculkan awan");
+            }
         }
     }
 }
