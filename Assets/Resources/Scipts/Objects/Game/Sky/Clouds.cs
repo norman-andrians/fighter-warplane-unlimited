9430376d588b0abe8d3c67f22d4b26fe8506362b
norman-andrians <norman25.na@gmail.com>
2021-09-03 13:52:19 +0700

    version: 0.14, memperbaiki spawn awan fase-2 (selesai)

--- Assets/Resources/Scipts/Objects/Game/Sky/Clouds.cs
+++ Assets/Resources/Scipts/Objects/Game/Sky/Clouds.cs
@@ -1,7 +1,8 @@
 ﻿/* 
- * ini adalah skrip untuk menggerakkan awan
+ * ini adalah skrip untuk awan
  */
 
+using System.Collections;
 using UnityEngine;
 
 public class Clouds : MonoBehaviour
@@ -10,21 +11,55 @@ public class Clouds : MonoBehaviour
     public float minSpeed = 1f;
     public float maxSpeed = 3f;
 
+    // waktu mati
+    public float lifeTime = 6f;
+
     // kecepatan awan
     private float speed;
+
+    private bool isDead = false;
+
+    // warna render
+    private Renderer rend;
+    private Color warnaAwan;
 
     void Start()
     {
         // mengatur kecepatan random
         speed = Random.Range(minSpeed, maxSpeed);
+
+        StartCoroutine(hancurkanAwan());
+        alphaAwan();
+    }
+
+    // fucntion untuk mengubah warna dan opacity (alpha) untuk awan
+    private void alphaAwan()
+    {
+        warnaAwan = new Vector4(1, 1, 1, Random.Range(.5f, .8f));
+
+        rend = gameObject.GetComponent<Renderer>();
+        rend.material.color = warnaAwan;
     }
 
     private void FixedUpdate()
     {
-        // vector 2 menjalankan awan
-        Vector2 jalan = new Vector2(-speed, 0);
+        if (isDead)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            // vector 2 menjalankan awan
+            Vector2 jalan = new Vector2(-speed, 0);
 
-        // menjalankan awan
-        transform.Translate(jalan * Time.deltaTime);
+            // menjalankan awan
+            transform.Translate(jalan * Time.deltaTime);
+        }
+    }
+
+    private IEnumerator hancurkanAwan()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        isDead = true;
     }
 }
